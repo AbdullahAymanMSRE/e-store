e030f61bd99092ff32e17447284a8d549709085c
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2025-01-07 23:02:16 +0200

    remove seed

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -1,4 +1,4 @@
-import { getCachedGlobal } from '@/utilities/getGlobals'
+// import { getCachedGlobal } from '@/utilities/getGlobals'
 import Link from 'next/link'
 import React from 'react'
 
@@ -8,7 +8,7 @@ import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
 import { Logo } from '@/components/Logo/Logo'
 
 export async function Footer() {
-  const footerData: Footer = await getCachedGlobal('footer', 1)()
+  // const footerData: Footer = await getCachedGlobal('footer', 1)()
 
   return (
     <footer className="mt-auto border-t border-border bg-black dark:bg-card text-white">
